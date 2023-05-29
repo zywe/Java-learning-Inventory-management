6c85a0d181f2eb5e0214ba614f63fae19b235530
zywe <128295445+zywe@users.noreply.github.com>
2023-05-29 18:21:09 +0800

    AdminController

--- shop1.0/src/cn/network/product/controller/ProductController.java
+++ shop1.0/src/cn/network/product/controller/ProductController.java
@@ -59,97 +59,15 @@ public class ProductController extends ViewBaseServlet {
     }
 
 
-    private void content(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
 
         List<Product> productList = productDAO.getProduct();
         session.setAttribute("productList", productList);
 
-        super.processTemplate("content", request, response);
-    }
-
-
-    private void edit(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idStr = request.getParameter("id");
-        if (StringUtil.isNotEmpty(idStr)) {
-            int id = Integer.parseInt(idStr);
-            Product product = productDAO.getProductById(id);
-            request.setAttribute("product", product);
-            super.processTemplate("edit", request, response);
-
-        }
-    }
-
-
-    private void update(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        Integer id = Integer.parseInt(request.getParameter("id"));
-        String name = request.getParameter("name");
-        Double price = Double.parseDouble(request.getParameter("price"));
-        String img = request.getParameter("img");
-        productDAO.updateProduct(new Product(id, name, price, img));
-        response.sendRedirect("product.do");
-
-    }
-
-    private void del(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idStr = request.getParameter("id");
-        if (StringUtil.isNotEmpty(idStr)) {
-            int id = Integer.parseInt(idStr);
-            productDAO.delProduct(id);
-            response.sendRedirect("product.do");
-
-        }
-
-    }
-
-    private void add(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        String name = request.getParameter("name");
-        Double price = Double.parseDouble(request.getParameter("price"));
-        String img = request.getParameter("img");
-        Product product = new Product(0, name, price, img);
-        productDAO.addProduct(product);
-        response.sendRedirect("product.do");
-
-
-    }
-
-
-    private void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        Integer pageNo = 1;
-        String Keyword = null;
-        String oper = request.getParameter("oper");
-
-        if (StringUtil.isNotEmpty(oper) && "search".equals(oper)) {
-            pageNo = 1;
-            Keyword = request.getParameter("keyword");
-            if (StringUtil.isEmpty(Keyword)) {
-                Keyword = "";
-            }
-            session.setAttribute("keyword", Keyword);
-        } else {
-            String pageNoStr = request.getParameter("pageNo");
-            if (StringUtil.isNotEmpty(pageNoStr)) {
-                pageNo = Integer.parseInt(pageNoStr);
-            }
-            Object keywordObj = session.getAttribute("keyword");
-            if (keywordObj != null) {
-                Keyword = (String) keywordObj;
-            } else {
-                Keyword = "";
-            }
-        }
-
-        session.setAttribute("pageNo", pageNo);
-        List<Product> productList = productDAO.getProductList(Keyword, pageNo);
-        session.setAttribute("productList", productList);
-
-        int productCount = productDAO.getProductCount(Keyword);
-        int pageCount = (productCount + 5 - 1) / 5;
-        session.setAttribute("pageCount", pageCount);
-
         super.processTemplate("index", request, response);
     }
+
+
+
 }
--- shop1.0/web/js/index.js
+++ shop1.0/web/js/index.js
@@ -2,16 +2,20 @@ function add() {
     window.location.href = "add.html"
 }
 
+
+// product.do?operate=adminIndex&pageNo=3
+
+
 function delProduct(id) {
     if (confirm('是否确认删除？')) {
-        window.location.href = 'product.do?id=' + id + '&operate=del';
+        window.location.href = 'admin.do?id=' + id + '&operate=del';
     }
 }
 
 function edit(id) {
-    window.location.href = 'product.do?id=' + id + '&operate=edit';
+    window.location.href = 'admin.do?id=' + id + '&operate=edit';
 }
 
 function page(pageNo) {
-    window.location.href = "product.do?pageNo=" + pageNo;
+    window.location.href = "admin.do?pageNo=" + pageNo;
 }
